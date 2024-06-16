15f10e7611396f73d6e4232b1f163f29a936d03e
Agus Heryanto <agusheryantodev@gmail.com>
2024-06-16 22:56:58 +0700

    fix : fixing update product feature

--- app/api/v1/products/controller.js
+++ app/api/v1/products/controller.js
@@ -76,21 +76,38 @@ const deleteProductCms = async (req, res, next) => {
 
 const updateProductCms = async (req, res, next) => {
     const { id } = req.params;
-    const { name, price, category, imageUrl } = req.body;
+    const { name, price, category } = req.body;
+    // validation
+    await body('name').isString().withMessage('name must be a string').run(req);
+    await body('price').isNumeric().withMessage('price must be a number').run(req);
+    await body('category').isString().withMessage('category must be a string').run(req);
+    const errors = validationResult(req);
 
-    if (price <= 0) {
-        throw new customError.BadRequestError('price must be greater than 0');
-    }
-
-    if (category !== 'food' && category !== 'drink') {
-        throw new customError.BadRequestError('category must be food or drink');
-    }
-
-    if (!name || !price || !category || !imageUrl) {
-        throw new customError.BadRequestError('all fields are required');
-    }
     try {
-        const result = await productService.UpdateProduct(id, name, price, category, imageUrl);
+
+        if (price <= 0) {
+            throw new customError.BadRequestError('price must be greater than 0');
+        }
+
+        if (category !== 'food' && category !== 'drink') {
+            throw new customError.BadRequestError('category must be food or drink');
+        }
+
+        if (!name || !price || !category) {
+            throw new customError.BadRequestError('all fields are required');
+        }
+
+        if (!errors.isEmpty()) {
+            throw new customError.BadRequestError(errors.array()[0].msg);
+        }
+
+        const resultImages = await imageService.generateUrlImage({ file: req.file });
+
+        if (!resultImages) {
+            throw new customError.InternalServerError('failed to upload image');
+        }
+
+        const result = await productService.UpdateProduct(id, name, price, category, resultImages);
         res.status(200).json({
             data: result
         });
--- app/api/v1/products/router.js
+++ app/api/v1/products/router.js
@@ -7,6 +7,6 @@ const upload = require('../../../middlewares/multers');
 router.post('/admin/product/create', auth.authenticateUser, auth.authorizeRoles('admin'), upload.single('images'), productCtrl.createProductCms);
 router.get('/admin/product', auth.authenticateUser, auth.authorizeRoles('admin'), productCtrl.getAllProductsCms);
 router.delete('/admin/product/:id', auth.authenticateUser, auth.authorizeRoles('admin'), productCtrl.deleteProductCms);
-router.put('/admin/product/:id', auth.authenticateUser, auth.authorizeRoles('admin'), productCtrl.updateProductCms);
+router.put('/admin/product/:id', auth.authenticateUser, auth.authorizeRoles('admin'), upload.single('images'), productCtrl.updateProductCms);
 
 module.exports = router
