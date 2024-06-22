50bbbbc7e2f1a3237807c489e1094ff65d649fa4
Agus Heryanto <agusheryantodev@gmail.com>
2024-06-22 14:53:32 +0700

    feat : consuming API for retrieve hero data

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,64 +1,22 @@
 <template>
   <section id="home">
-    <div class="sec-wp">
-      <div class="container">
-        <div class="row" id="gallery">
-          <div class="col-lg-10 m-auto">
-            <div class="book-table-img-slider" id="icon">
-              <div class="swiper-wrapper">
-                <a
-                  href="/src/assets/images/bt1.jpg"
-                  data-fancybox="table-slider"
-                  class="book-table-img back-img swiper-slide"
-                  style="background-image: url(/src/assets/images/bt1.jpg)"
-                ></a>
-                <a
-                  href="/src/assets/images/bt2.jpg"
-                  data-fancybox="table-slider"
-                  class="book-table-img back-img swiper-slide"
-                  style="background-image: url(/src/assets/images/bt2.jpg)"
-                ></a>
-                <a
-                  href="/src/assets/images/bt3.jpg"
-                  data-fancybox="table-slider"
-                  class="book-table-img back-img swiper-slide"
-                  style="background-image: url(/src/assets/images/bt3.jpg)"
-                ></a>
-                <a
-                  href="/src/assets/images/bt4.jpg"
-                  data-fancybox="table-slider"
-                  class="book-table-img back-img swiper-slide"
-                  style="background-image: url(/src/assets/images/bt4.jpg)"
-                ></a>
-                <a
-                  href="/src/assets/images/bt1.jpg"
-                  data-fancybox="table-slider"
-                  class="book-table-img back-img swiper-slide"
-                  style="background-image: url(/src/assets/images/bt5.jpg)"
-                ></a>
-                <a
-                  href="/src/assets/images/bt2.jpg"
-                  data-fancybox="table-slider"
-                  class="book-table-img back-img swiper-slide"
-                  style="background-image: url(/src/assets/images/bt6.jpg)"
-                ></a>
-                <a
-                  href="/src/assets/images/bt3.jpg"
-                  data-fancybox="table-slider"
-                  class="book-table-img back-img swiper-slide"
-                  style="background-image: url(/src/assets/images/bt7.jpg)"
-                ></a>
-                <a
-                  href="/src/assets/images/bt4.jpg"
-                  data-fancybox="table-slider"
-                  class="book-table-img back-img swiper-slide"
-                  style="background-image: url(/src/assets/images/bt8.jpg)"
-                ></a>
-              </div>
+    <div class="container">
+      <div class="row" id="gallery">
+        <div class="col-lg-100 m-auto">
+          <div class="book-table-img-slider swiper-container" id="icon">
+            <div class="swiper-wrapper">
+              <a
+                v-for="image in images"
+                :key="image.id"
+                :href="image.url"
+                data-fancybox="table-slider"
+                class="book-table-img back-img swiper-slide"
+                :style="{ backgroundImage: `url(${image.url})` }"
+              ></a>
             </div>
-            <div class="book-table-now">
-              <a @click.prevent="openNewTab" class="book-table-link">Buy Now</a>
-            </div>
+          </div>
+          <div class="book-table-now">
+            <a @click.prevent="openNewTab" class="book-table-link">Buy Now</a>
           </div>
         </div>
       </div>
@@ -71,25 +29,11 @@
     </div>
     <div class="container">
       <div class="row">
-        <div class="col-lg-4 col-md-6">
+        <div v-for="offer in offerList" :key="offer.id" class="col-lg-4 col-md-6">
           <div class="offer-item">
-            <img src="/src/assets/images/off2.jpeg" class="img-fluid" alt="" />
-            <h3>Sushi Special Menu?</h3>
-            <p>Yes it is.</p>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <div class="offer-item">
-            <img src="/src/assets/images/off1.jpeg" class="img-fluid" alt="" />
-            <h3>Sushi Dinner</h3>
-            <p>You can also dinner with sushi with discounts.</p>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <div class="offer-item">
-            <img src="/src/assets/images/off3.jpeg" class="img-fluid" alt="" />
-            <h3>Every Day Is Sushi</h3>
-            <p>Nice Choice.</p>
+            <img :src="offer.imageUrl" class="img-fluid" :alt="offer.title" />
+            <h3>{{ offer.title }}</h3>
+            <p>{{ offer.description }}</p>
           </div>
         </div>
       </div>
@@ -117,19 +61,96 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
-import { getMenuItems } from '../services/menuService'
+import { ref, onMounted, nextTick } from 'vue'
+import axios from '../axios.js'
 
 export default {
   setup() {
     const menuItems = ref([])
+    const offerList = ref([])
+    const images = ref([])
+    const token = localStorage.getItem('token')
+    const baseUrl = axios.defaults.baseURL
 
-    onMounted(async () => {
+    const fetchMenuItems = async () => {
       try {
-        menuItems.value = await getMenuItems()
+        const response = await axios.get('/api/v1/product', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        menuItems.value = response.data.data.map((item) => {
+          item.imageUrl = `${baseUrl}/${item.imageUrl}`
+          return item
+        })
       } catch (error) {
         console.error('Error fetching menu items:', error)
       }
+    }
+
+    const fetchOfferList = async () => {
+      try {
+        const response = await axios.get('/api/v1/offer', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        offerList.value = response.data.data.map((offer) => {
+          offer.imageUrl = `${baseUrl}/${offer.imageUrl}`
+          return offer
+        })
+      } catch (error) {
+        console.error('Error fetching offer list:', error)
+      }
+    }
+
+    const fetchHeroImages = async () => {
+      try {
+        const response = await axios.get('/api/v1/hero', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        images.value = response.data.data.map((image) => {
+          image.url = `${baseUrl}/${image.imageUrl}`
+          return image
+        })
+        nextTick(() => {
+          // Inisialisasi Swiper dengan jQuery
+          // eslint-disable-next-line no-unused-vars, no-undef
+          /* eslint-disable no-undef */
+          /* eslint-disable no-unused-vars */
+          $(document).ready(function ($) {
+            var book_table = new Swiper('.book-table-img-slider', {
+              slidesPerView: 1,
+              spaceBetween: 20,
+              loop: true,
+              autoplay: {
+                delay: 2000,
+                disableOnInteraction: false
+              },
+              speed: 2000,
+              effect: 'coverflow',
+              coverflowEffect: {
+                rotate: 2,
+                stretch: 2,
+                depth: 100,
+                modifier: 5,
+                slideShadows: true
+              },
+              loopAdditionSlides: true
+            })
+          })
+        })
+      } catch (error) {
+        console.error('Error fetching hero images:', error)
+      }
+    }
+
+    onMounted(() => {
+      fetchMenuItems()
+      fetchOfferList()
+      fetchHeroImages()
     })
 
     const openNewTab = () => {
@@ -138,6 +159,8 @@ export default {
 
     return {
       menuItems,
+      offerList,
+      images,
       openNewTab
     }
   }
